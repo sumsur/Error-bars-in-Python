792de72286c772ffcdd06b44db01969b0e2acaaa
Tatjana Vujovic <63498938+sumsur@users.noreply.github.com>
2020-04-18 17:04:00 +0200

    Update errorbars.py

--- errorbars.py
+++ errorbars.py
@@ -1,13 +1,14 @@
-import pandas as pd
-import numpy as np
+import pandas as pd   
+import numpy as np  #packages for importing data as DataFrame
 from numpy import median
 
 import seaborn as sns
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt#packages for creating plots
 
 asd = pd.read_excel(r'C:\Users\asd.xlsx', sheet_name='Sheet1')
-asdd = pd.DataFrame(asd, columns= ['Mesec','Broj zahteva poslednjeg dana u mesecu'])
+asdd = pd.DataFrame(asd, columns= ['Mesec','Broj zahteva poslednjeg dana u mesecu']) #database of the Danube insurance company 
 
-sns.barplot(x='Mesec', y='Broj zahteva poslednjeg dana u mesecu', data=asdd, estimator=median, capsize=0.3)
+sns.barplot(x='Mesec', y='Broj zahteva poslednjeg dana u mesecu', data=asdd, estimator=median, capsize=0.3) #creating barplot-visualizing error bars for each claim thah has been received at the end of month
 
 plt.show()
+
